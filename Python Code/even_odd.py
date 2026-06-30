6d029c20dba52a2b95811d62d2d8b1c0e18eae2d
ShadowFighter00947 <patelamjadali21@gmail.com>
2026-06-30 11:33:30 +0530

    Even or Odd

---
+++ Python Code/even_odd.py
@@ -0,0 +1,7 @@
+num = int(input('Please enter a number:-'))
+# Getting input from user and converting it into an integer.
+
+if (num%2==0):
+    print('Given number is even.')
+else:
+    print('Given number is odd.')
