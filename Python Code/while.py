cd0b893469d67b44877893d7e5d30102f4b26530
ShadowFighter00947 <patelamjadali21@gmail.com>
2026-07-03 20:25:16 +0530

    Use of while

---
+++ Python Code/while.py
@@ -0,0 +1,10 @@
+print("In which year India got it\'s independence...?")
+year = int(input())
+# int(input()) takes answer from the user.
+
+while (year != 1947):
+    print("Comeon you don\'t no that much, damm..!")
+    print("Sigh.., fine i will give you another chance.")
+    year = int(input())
+
+print("Finally...! you got it right, well done.!")
